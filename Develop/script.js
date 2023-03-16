0f53314a4ca84a5a7c60d9286dfdc99f245e1ab6
Monique <mo.sherred@gmail.com>
2023-03-15 17:46:54 -0700

    Figured it out mostly

--- Develop/script.js
+++ Develop/script.js
@@ -1,116 +1,119 @@
+
 $(document).ready(function () {
 
-  var today = dayjs();
+  var today = dayjs()
   var formattedDate = dayjs(today).format('dddd, MMMM D, YYYY h:mm A')
-  $("header #currentDay").text("Today's date is " + formattedDate);
+  $("header #currentDay").text("Today's date is " + formattedDate)
 
   $(".saveBtn").on("click", function () {
-    var timeBlockId = $(this).closest(".time-block").attr("id");
-    var textAreaValue = $(this).siblings(".description").val();
-    localStorage.setItem(timeBlockId, textAreaValue);
+    // 'this' refers to the button element that was clicked, 
+    // and closest(".time-block") looks for the closest ancestor 
+    // of that element that has the class "time-block". 
+    // The attr("id") method is then used to get the value 
+    // of the id attribute of that element.
+    var timeBlockId = $(this).closest(".time-block").attr("id")
+    var textAreaValue = $(this).siblings(".description").val()
+    localStorage.setItem(timeBlockId, textAreaValue)
+    console.log(timeBlockId, textAreaValue)
   });
 
   function updateTimeBlocks() {
     $(".time-block").each(function () {
-      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
-      var currentHour = dayjs().hour(); // declare currentHour inside updateTimeBlocks
-  
+      var currentHour = dayjs().hour()
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1])
+      // 'this' is referring to each individual '.time-block' 
+      // element that is being iterated over by the 'each()' method, 
+      // and '$(this).attr("id")' gets the id attribute of 
+      // that element. The 'split("-")[1]' method is then used to
+      //  get the second part of the id attribute 
+      //  (which is separated by a hyphen), 
+      //  and 'parseInt()' is used to convert it to an integer.
+      console.log('current hour:', currentHour);
+
       if (timeBlockHour > currentHour) {
-        $(this).addClass("future").removeClass("past present");
-      } else if (timeBlockHour === currentHour) {
-        $(this).addClass("present").removeClass("past future");
-      } else {
-        $(this).addClass("past").removeClass("present future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        // 'this' refers to each individual
+        // '.time-block' element being iterated over by the 
+        // 'each()' method. The'addClass()' and 'removeClass()' methods 
+        // are used to modify the classes on the element, adding or 
+        // removing the classes ".future", ".past", and ".present" 
+        // as appropriate.
       }
-    });
-
-    $(".time-block").each(function () {
-      var timeBlockId = $(this).attr("id");
-      var savedText = localStorage.getItem(timeBlockId);
-      if (savedText) {
-        $(this).find(".description").val(savedText);
+      else if (timeBlockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");;
       }
-    });
+      else {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
 
-    setInterval(updateTimeBlocks, 60000);
+    })
   }
 
-  updateTimeBlocks(); // call updateTimeBlocks once to initialize
-}); 
 
+  $(".time-block").each(function () {
+    // 'this' refers to each '.time-block'
+    var timeBlockId = $(this).attr("id")
+    var savedText = localStorage.getItem(timeBlockId)
+    if (savedText) {
+      $(this).find(".description").val(savedText)
+    }
+  })
+  // ! it takes a minute to load correctly I'm not sure why
+  setInterval(updateTimeBlocks, 10000)
+})
 
-// ! Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// ! the code isn't run until the browser has finished rendering all the elements
-// ! in the html.
+/**========================================================================
+ * *                                INFO
+ *
+ * ?                           I know nothing
+ * ?                           here are words
+ *
+ *========================================================================**/
 
-  // TODO: Add a listener for click events on the save button. This code should
-  //? use the id in the containing time-block as a key to save the user input in
-  //? local storage. HINT: What does `this` reference in the click listener
-  //? function? How can DOM traversal be used to get the "hour-x" id of the
-  //? time-block containing the button that was clicked? How might the id be
-  //? useful when saving the description in local storage?
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  //? block by comparing the id to the current hour. HINTS: How can the id
-  //? attribute of each time-block be used to conditionally add or remove the
-  //? past, present, and future classes? How can Day.js be used to get the
-  //? current hour in 24-hour time?
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  //? the values of the corresponding textarea elements. HINT: How can the id
-  // ?attribute of each time-block be used to do this?
-  
+// parseInt(): A JavaScript function that parses a string argument and returns an integer.
 
+// each(): A jQuery method that iterates over a collection of elements and executes a 
+// function for each element
 
+// addClass(): A jQuery method that adds one or more classes to the selected elements. 
 
+// removeClass(): A jQuery method that removes one or more classes from the selected elements. 
 
+// find(): A jQuery method that searches for descendants of the selected elements that match 
+// a specified selector.
 
-//* The $(document) selector selects the entire HTML document, 
-//*  and the .ready() function is called on it to indicate that we're waiting for 
-//*  the document to be ready.
+// val(): A jQuery method that gets or sets the value of form fields. 
 
-//*  By wrapping your code in this call to jQuery, 
-//*  you can ensure that your code won't try to manipulate the DOM 
-//*  until it's fully loaded and ready to be manipulated.
+// attr(): A jQuery method that gets or sets the value of an attribute on the selected elements.
 
+// split(): A JavaScript method that splits a string into an array of substrings based 
+// on a specified separator.
 
-// This code is a template for a scheduling application where the user can save tasks 
-// for different times of the day. The code is written in jQuery and has several 
-// task items that need to be completed. Let's break them down:
+// $ : shorthand for the jQuery object, which is a JavaScript library that makes it 
+// easier to work with HTML documents
 
+// $(document): A jQuery function that returns a jQuery object representing the entire 
+// HTML document.
 
+// ready(): A jQuery method that specifies a function to execute when the document is fully loaded.
 
+// localStorage: A JavaScript object that provides a way to store key-value pairs in 
+// the browser. Data stored in localStorage persists even after the browser is closed.
 
-// TODO: Add a listener for click events on the save button.
-// The first TODO item is asking you to add a click event listener to the save button. 
-// When the user clicks on the save button, the code should use the id attribute of 
-// the containing time-block as a key to save the user's input in local storage. 
-// The 'this' keyword in the click listener function refers to the save button that was clicked. 
-// You can use DOM traversal to get the id of the containing time-block by finding 
-// the closest parent element with a specific class, such as 
-// $(this).closest(".time-block").attr("id").
+// setItem(): A method of the localStorage object that sets a key - value pair.
 
+// closest(): A jQuery method that searches for the closest ancestor element that 
+// matches a specified selector.
 
+// siblings(): A jQuery method that returns all sibling elements of the selected elements.
 
-// TODO: Add code to apply the past, present, or future class to each time block.
-// The second TODO item is asking you to add code to apply the past, present, or future class 
-// to each time-block. This is based on comparing the id of the time-block to the current hour. 
-// You can use the id attribute of each time-block to conditionally add or remove the past, 
-// present, and future classes. You can use Day.js library to get the current hour in 24-hour time.
-
-
-
-
-// TODO: Add code to get any user input that was saved in localStorage.
-// The third TODO item is asking you to add code to get any user input that was previously 
-// saved in local storage and set the values of the corresponding textarea elements. 
-// You can use the id attribute of each time-block to retrieve the saved value from local storage.
-
-
-
-// TODO: Add code to display the current date in the header of the page.
-// The last TODO item is asking you to add code to display the current date in the 
-// header of the page. You can use Day.js library to get the current date and format it as desired, 
-// then use jQuery to set the text of an element in the header with the current date.
+// text(): A method that is used to retrieve the text content of an element, or set 
+// the text content of an element.
 
 
